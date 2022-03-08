dd0386137fb24c3875928052e7488e800ad5d27a
Abdukarimxonov G'aybullo <92791349+Abdukarimxonov@users.noreply.github.com>
2022-03-08 18:54:22 +0500

    18.54 08.03.2022

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ function openNav() {
 
 
 function change(x) {
-   x.classList.toggle("change");
+   x.classList.add("change");
 };
 /*------------*/
 const swiper = new Swiper('.swiper', {
@@ -30,3 +30,31 @@ const swiper = new Swiper('.swiper', {
       loadPrevNext: true,
    },
 });
+const iconMenu = document.querySelector('.container');
+const menuBody = document.querySelector('#mySidenav');
+const menuLinks = document.querySelectorAll('.menu_link[data-goto]');
+if (menuLinks.length > 0) {
+   menuLinks.forEach(menuLink => {
+      menuLink.addEventListener("click", onMenuLinkClick);
+   });
+
+   function onMenuLinkClick(e) {
+      const menuLink = e.target;
+      if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
+         const gotoBlock = document.querySelector(menuLink.dataset.goto);
+         const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset;
+
+         if (iconMenu.classList.contains('change')) {
+            document.body.classList.remove('lock');
+            iconMenu.classList.remove('change');
+            menuBody.classList.remove('active');
+         }
+
+         window.scrollTo({
+            top: gotoBlockValue,
+            behavior: "smooth"
+         });
+         e.preventDefault();
+      }
+   }
+}
